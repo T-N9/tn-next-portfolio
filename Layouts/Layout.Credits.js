d5709795e5af54e365e1354a5ca8665b4d5becfc
T-N9 <thethoriumtension@gmail.com>
2022-12-11 20:38:02 +0630

    [CreditPage] reduxState

--- Layouts/Layout.Credits.js
+++ Layouts/Layout.Credits.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect /*  */} from "react";
 import Transitions from "../components/Animated/Transitions";
+
+import { useDispatch } from "react-redux";
+import { setCurrentPage } from "../store/slices/NavbarSlice";
 
 /* Components */
 import { AllCredits } from "../components";
 
 const CreditPageLayout = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setCurrentPage("Home"));
+  }, []);
+
   return (
     <Transitions>
       <AllCredits />
