96d13618ad43f4b1d23808576cf473301d0f051a
T-N9 <thethoriumtension@gmail.com>
2022-12-11 20:47:57 +0630

    [ProjectDetail] Meta updated

--- pages/works/[slug].js
+++ pages/works/[slug].js
@@ -9,6 +9,48 @@ const ProjectDetail = ({ project, slug }) => {
   return (
     <>
       <Head>
+        <meta charset="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#1192d3" />
+        <meta name="title" content={project?.title} />
+        <meta name="description" content={project?.description} />
+        <meta
+          name="keywords"
+          content="Te Nyain Moe Lwin, TeNyain, Moe Lwin, Moe, web, web developer, web development, front-end, UI, Myanmar, junior web developer, job, freelance, promoting, branding, product branding, creative"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="TeNyain Moe Lwin" />
+        <meta property="og:locale" content="en-US" />
+
+        {/* <!-- Primary Meta Tags --> */}
+        <title>About TeNyain | Who am I?</title>
+        <meta name="title" content={project?.title} />
+        <meta name="description" content={project?.description} />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={`https://www.tenyain.com/${slug}`} />
+        <meta property="og:title" content={project?.title} />
+        <meta property="og:description" content={project?.description} />
+        <meta property="og:image" content="meta-tn.png" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content={`https://www.tenyain.com/${slug}`}
+        />
+        <meta property="twitter:title" content={project?.title} />
+        <meta property="twitter:description" content={project?.description} />
+        <meta property="twitter:image" content="meta-tn.png" />
+
+        <meta
+          name="google-site-verification"
+          content="nstIYPUM8pyaUUrW69SvgmJkxRRe_hS9tN_VAfzoLeI"
+        />
         <title>{project?.title}</title>
       </Head>
       <main>
