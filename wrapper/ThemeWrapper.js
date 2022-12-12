56fbf3199697a969a119afda175d61ca709c7ef4
T-N9 <thethoriumtension@gmail.com>
2022-12-12 20:44:16 +0630

    [Theme Class] updated

--- wrapper/ThemeWrapper.js
+++ wrapper/ThemeWrapper.js
@@ -2,16 +2,12 @@ import React from "react";
 import { useTheme } from "next-themes";
 
 const ThemeWrapper = (props) => {
-
   const { theme } = useTheme();
+  console.log(theme === "dark");
 
   return (
     <>
-        <main
-          className={theme === "dark" ? "main_content dark" : "main_content"}
-        >
-          {props.children}
-        </main>
+      <main className="main_content">{props.children}</main>
     </>
   );
 };
