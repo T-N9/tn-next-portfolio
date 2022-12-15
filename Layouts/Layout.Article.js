2a01ebc0105798c3177a5e179986425e6469b5d1
T-N9 <thethoriumtension@gmail.com>
2022-12-15 15:46:23 +0630

    [Article] route active

--- Layouts/Layout.Article.js
+++ Layouts/Layout.Article.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Transitions from "../components/Animated/Transitions";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentPage } from "../store/slices/NavbarSlice";
 
 /* Components */
 import { ArticleDetail } from "../components";
 
 const ArticlePageLayout = ({ data, slug }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setCurrentPage("Writing"));
+  }, []);
   return (
     <Transitions>
       <ArticleDetail data={data} slug={slug} />
