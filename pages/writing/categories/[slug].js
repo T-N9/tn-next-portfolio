c5839e07ffce77aa0445dab38af04ce6d188ea63
T-N9 <thethoriumtension@gmail.com>
2022-12-15 10:34:26 +0630

    [Category] meta link updated

--- pages/writing/categories/[slug].js
+++ pages/writing/categories/[slug].js
@@ -78,7 +78,10 @@ const SearchByCategory = () => {
 
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.tenyain.com/writing/" />
+        <meta
+          property="og:url"
+          content={`https://www.tenyain.com/writing/categories/${slug}`}
+        />
         <meta
           property="og:title"
           content={`${currentCategory?.title} | Articles by category`}
@@ -93,7 +96,7 @@ const SearchByCategory = () => {
         <meta property="twitter:card" content="summary_large_image" />
         <meta
           property="twitter:url"
-          content="https://www.tenyain.com/writing/"
+          content={`https://www.tenyain.com/writing/categories/${slug}`}
         />
         <meta
           property="twitter:title"
