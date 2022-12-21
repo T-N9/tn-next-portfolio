d4199fa3e9201016905a586cef32bfd974aaee73
T-N9 <thethoriumtension@gmail.com>
2022-12-21 10:47:15 +0630

    [Loading] Trans line added

--- components/GlobalLoad/index.js
+++ components/GlobalLoad/index.js
@@ -5,9 +5,13 @@ import Logo from "../Navbar/Logo";
 const GlobalLoading = ({ loading, isLogo, isFetching }) => {
   return (
     <div className={loading ? "loading_page visible" : "loading_page hidden"}>
-      {isFetching && <i className={`gg-${"spinner"}`}></i>
-}
-      {isLogo && <Logo />}
+      {isFetching && <i className={`gg-${"spinner"}`}></i>}
+      {isLogo && (
+        <div className="loading-trans">
+          <Logo />
+          <div className="loading-line"></div>
+        </div>
+      )}
     </div>
   );
 };
