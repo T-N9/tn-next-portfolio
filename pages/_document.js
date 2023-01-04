59015f10e427e05108d3dc82018dd94df9b38c1f
T-N9 <thethoriumtension@gmail.com>
2023-01-04 15:51:12 +0630

    [Messenger] test

--- pages/_document.js
+++ pages/_document.js
@@ -33,6 +33,40 @@ export default function Document() {
         />
       </Head>
       <body>
+        {/* <!-- Messenger Chat plugin Code --> */}
+        <div id="fb-root"></div>
+
+        {/* <!-- Your Chat plugin code --> */}
+        <div id="fb-customer-chat" class="fb-customerchat"></div>
+        <Script
+          id="messenger-tag"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `      var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "107118861736913");
+            chatbox.setAttribute("attribution", "biz_inbox");`,
+          }}
+        ></Script>
+        <Script
+          id="messenger-sdk"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: ` window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v15.0'
+              });
+            };
+      
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));`,
+          }}
+        ></Script>
         <Main />
         <NextScript />
       </body>
