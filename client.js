07dfd5cc0e4d9095bb54dc05ff0234819619c597
T-N9 <thethoriumtension@gmail.com>
2023-02-05 10:35:07 +0630

    [SanityClient] useCdn false

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ export const client = sanityClient({
   projectId: process.env.NEXT_PUBLIC_REACT_APP_SANITY_PROJECT_ID ,
   dataset: 'production',
   apiVersion: '2022-02-24',
-  useCdn: true,
+  useCdn: false,
   token: process.env.NEXT_PUBLIC_REACT_APP_SANITY_TOKEN,
 });
 
