3280c6e171f0c9f2ee52944509611f6c58f781e0
T-N9 <thethoriumtension@gmail.com>
2023-02-22 20:48:58 +0630

    [ContactPage] linkedIn test

---
+++ components/HomeContact/LinkedIn.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const LinkedIn = () => {
+  return (
+    <section className="linkedIn_wrapper container_sm container_y_3">
+      <div className="linkedIn--grid">
+        <iframe
+          src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7034064880357822464"
+          height="auto"
+          width="auto"
+          frameborder="0"
+          allowfullscreen=""
+          title="Embedded post"
+          data-embed-uuid="7034064880357822464"
+        ></iframe>
+        <iframe
+          src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7031794428130332673"
+          height="auto"
+          width="auto"
+          frameborder="0"
+          allowfullscreen=""
+          title="Embedded post"
+          data-embed-uuid="7031794428130332673"
+        ></iframe>
+
+        <iframe
+          src="https://www.linkedin.com/embed/feed/update/urn:li:share:6860842375053504512"
+          height="auto"
+          width="auto"
+          frameborder="0"
+          allowfullscreen=""
+          title="Embedded post"
+          data-embed-uuid="6860842375053504512"
+        ></iframe>
+      </div>
+    </section>
+  );
+};
+
+export default LinkedIn;
--- components/HomeContact/index.js
+++ components/HomeContact/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ContactImg from './ContactImg';
-import ContactForm from './ContactForm';
+import React from "react";
+import ContactImg from "./ContactImg";
+import ContactForm from "./ContactForm";
+import LinkedIn from "./LinkedIn";
 
 const HeroContact = () => {
-    return (
-        <section className="contact_page_wrapper">
-            <div className="contact_page">
-                <ContactImg/>
-                <ContactForm/>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section className="contact_page_wrapper">
+      <div className="contact_page">
+        <ContactImg />
+        <ContactForm />
+      </div>
+
+      <LinkedIn/>
+    </section>
+  );
+};
 
 export default HeroContact;
