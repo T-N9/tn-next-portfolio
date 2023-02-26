d47db8dba7551798198a68e39a95266e7b6a2378
T-N9 <thethoriumtension@gmail.com>
2023-02-26 22:11:31 +0630

    [HomeContact] Dir updated

--- components/HomeContact/ContactImg.jsx
+++ components/HomeContact/ContactImg.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import MailBox from "../../assets/mailBox.svg";
-import MailBoxDark from "../../assets/mailBox-dark.svg";
 
 const ContactImg = () => {
   return (
@@ -8,14 +6,14 @@ const ContactImg = () => {
       <div className="contact_img">
         <img
           id="light-box"
-          src="/mailBox.svg"
+          src="/content/mailBox.svg"
           alt="mailbox"
           width="429.19px"
           height="312.41px"
         />
         <img
           id="dark-box"
-          src="mailBox-dark.svg"
+          src="/content/mailBox-dark.svg"
           alt="mailbox"
           width="429.19px"
           height="312.41px"
