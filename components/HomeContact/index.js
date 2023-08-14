4f23b4542f3808dc2a7172f47c898dfdcba9f1db
T-N9 <thethoriumtension@gmail.com>
2023-08-14 13:41:35 +0630

    [HomeProject] design updated

--- components/HomeContact/index.js
+++ components/HomeContact/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import ContactImg from "./ContactImg";
 import ContactForm from "./ContactForm";
 
+import ScrollRevealDiv from '../common/ScrollRevealDiv';
+
 const HeroContact = () => {
   return (
     <section className="contact_page_wrapper">
-      <div className="contact_page">
-        <ContactImg />
-        <ContactForm />
-      </div>
+      <ScrollRevealDiv>
+        <div className="contact_page">
+          <ContactImg />
+          <ContactForm />
+        </div>
+      </ScrollRevealDiv>
     </section>
   );
 };
